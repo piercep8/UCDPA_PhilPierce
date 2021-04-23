ca5615097c271b90bccda083931374371a4dc628
piercep8 <81259260+piercep8@users.noreply.github.com>
2021-04-23 10:23:00 +0100

    Add files via upload

--- Football Main Program.py
+++ Football Main Program.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 
 # Simplified Position Dictionary - to be used to convert the various midfield positions a generic midfield role
-simple_position_dict = {'A': 'A', 'AM': 'M', 'G': 'G', 'D': 'D', 'DM': 'M', 'M': 'M', 'W': 'M'}
+simple_position_dict = {'A': '4: FWD', 'AM': '3: MID', 'G': '1: GK', 'D': '2: DEF', 'DM': '3: MID', 'M': '3: MID', 'W': '3: MID'}
 
 fifa_country_list = ['Netherlands', 'USA', 'IR Iran', 'Korea Republic', 'Republic of Ireland',
                      'Northern Ireland', 'Bosnia and Herzegovina', 'Côte dIvoire', 'Congo DR', 'Cabo Verde',
@@ -30,8 +30,8 @@ fifa_rankings_df = fifa_rankings_df.rename(columns={'country_full': 'Nation'})
 latest_rank_date = fifa_rankings_df['rank_date'].max()
 fifa_rankings_df = fifa_rankings_df[fifa_rankings_df['rank_date'] == latest_rank_date]
 
-# Create a list of just the countries and their ranking
-fifa_countries_df = fifa_rankings_df[['Nation', 'rank']]
+# Create a list of just the countries, their ranking and short_name
+fifa_countries_df = fifa_rankings_df[['Nation', 'rank', 'country_abrv']]
 
 #                                 ------ Import and Format Player Data File -----
 # Import CSV File and set the first row as the header. Store as a Pandas Data Frame
@@ -62,6 +62,10 @@ master_football_df['Name'] = master_football_df['Name'].replace("\t", " ", regex
 # Simplify the positions so Wingers, Defensive Midfielders and Attacking Midfielders are all grouped as Midfielders
 master_football_df['Simple Position'] = master_football_df['Best Position'].map(simple_position_dict)
 
+# Split the Simple Position column to produce a position order column. So "1: GK" becomes "1" and "GK"
+master_football_df[['position order', 'Simple Position']] = master_football_df['Simple Position'].str.split(':', 1, expand=True)
+master_football_df['Simple Position'] = master_football_df['Simple Position'].apply(lambda x: x.strip())
+
 # Change the names of Nations in the Master Football Data Frame to match those on the FIFA World Rankings Data Frame
 i = len(master_country_list)
 for country in range(i):
@@ -72,21 +76,21 @@ for country in range(i):
 master_football_df = master_football_df.merge(fifa_countries_df, how='left')
 
 # Sort the dataframe into Nation (A-Z), Simple Position (A-Z) and Scout Rating (Highest to Lowest)
-master_football_df = master_football_df.sort_values(['rank', 'Nation', 'Simple Position', 'Scout Rating'],
+master_football_df = master_football_df.sort_values(['rank', 'Nation', 'position order', 'Scout Rating'],
                                                     ascending=[True, True, True, False])
 
 # Produce a subset of the Master Football DF based on player position
-goalkeepers_df = master_football_df[(master_football_df['Simple Position'] == 'G')]
-defenders_df = master_football_df[(master_football_df['Simple Position'] == 'D')]
-midfielders_df = master_football_df[(master_football_df['Simple Position'] == 'M')]
-attackers_df = master_football_df[(master_football_df['Simple Position'] == 'A')]
+goalkeepers_df = master_football_df[(master_football_df['Simple Position'] == 'GK')]
+defenders_df = master_football_df[(master_football_df['Simple Position'] == 'DEF')]
+midfielders_df = master_football_df[(master_football_df['Simple Position'] == 'MID')]
+attackers_df = master_football_df[(master_football_df['Simple Position'] == 'FWD')]
 
 # Create a new dataframe to hold my squad lists for the top 50 teams based on current FIFA World Rankings
 squad_list_df = pd.DataFrame()
-ave_rating_df = pd.DataFrame([['Nation', 'Simple Position', 'Scout Rating', 'Ave Rating']])
+ave_rating_df = pd.DataFrame([['rank', 'country_abrv','position order', 'Simple Position', 'Scout Rating', 'Ave Rating']])
 
 # Loop through our position based dataframes and for each team in the FIFA top 50, produce a squad based on
-for team in range(3):
+for team in range(11):
     temp_goalkeepers_df = goalkeepers_df[(goalkeepers_df['rank'] == team)]
     temp_goalkeepers_df = temp_goalkeepers_df.head(3)
     squad_list_df = squad_list_df.append(temp_goalkeepers_df)
@@ -97,8 +101,22 @@ for team in range(3):
     temp_attackers_df = attackers_df[(attackers_df['rank'] == team)]
     squad_list_df = squad_list_df.append(temp_attackers_df.head(5))
 
-# Create a dataframe with the average rating for each squad for each position
-ave_rating_df = squad_list_df.groupby(['Nation', 'Simple Position'], as_index=False)['Scout Rating'].mean()
+squad_list_df = squad_list_df.sort_values(['rank', 'country_abrv', 'position order', 'Scout Rating'],
+                                          ascending=[True, True, True, False])
+
+# Datasets for graph plotting
+# Create a table with the average rating for each squad for each position
+ave_rating_df = squad_list_df.groupby(['rank', 'country_abrv', 'position order', 'Simple Position'],
+                                      as_index=False)['Scout Rating'].mean()
+
+# Build Graphs
+country_col = {1: 'red', 2: 'orangered', 3: 'orange', 4: 'gold', 5: 'yellow', 6: 'greenyellow', 7: 'forestgreen',
+               8: 'teal', 9: 'royalblue', 10: 'midnightblue'}
+
+#import matplotlib.pyplot as plt
+
+
+
 
 # Get a list of all nations and a list of all positions
 best_pos_values = master_football_df['Best Position'].unique()
